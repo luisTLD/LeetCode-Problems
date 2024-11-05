92f871a161e8a4f0ffbd6b15b32f555f14419727
Luis Toledo <141417786+luisTLD@users.noreply.github.com>
2024-11-05 16:04:14 -0300

    day

---
+++ 2914. Minimum Number of Changes to Make Binary String Beautiful/Solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minChanges(self, s: str) -> int:
+        y = len(s)
+        x = 0
+        for i in range(1, y, 2):
+            if s[i] != s[i - 1]:
+                x += 1
+        return x
