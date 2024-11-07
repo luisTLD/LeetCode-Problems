0fc1feb122cdb8110b34a7732e70789656fe4020
Luis Toledo <141417786+luisTLD@users.noreply.github.com>
2024-11-07 10:58:15 -0300

    easy

---
+++ 144. Binary Tree Preorder Traversal/Solution.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        self._preorder(root, result)
+        return result
+    
+    def _preorder(self, node: Optional[TreeNode], result: List[int]):
+        if node:
+            result.append(node.val)
+            self._preorder(node.left, result)
+            self._preorder(node.right, result)
