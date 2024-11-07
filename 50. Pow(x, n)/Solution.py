5c3d956c9fffbde5f36680b03658fea8b3493bb1
Luis Toledo <141417786+luisTLD@users.noreply.github.com>
2024-11-07 20:19:22 -0300

    easy

---
+++ 50. Pow(x, n)/Solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        result = 1
+        is_negative = n < 0
+        n = abs(n)
+
+        while n > 0:
+            if n % 2 == 1:
+                result *= x
+            x *= x
+            n //= 2
+
+        return 1 / result if is_negative else result
