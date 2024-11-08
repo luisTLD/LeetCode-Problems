d95e235736cf1144bb5bdd4ed77ae04f4e51dd8b
Luis Toledo <141417786+luisTLD@users.noreply.github.com>
2024-11-07 21:32:45 -0300

    medium

--- 2275. Largest Combination With Bitwise AND Greater Than Zero/Solution.py
+++ 2275. Largest Combination With Bitwise AND Greater Than Zero/Solution.py
@@ -1,7 +1,7 @@
 class Solution:
     def largestCombination(self, candidates: List[int]) -> int:
         max_combination = 0
-        for bit in range(32):
+        for bit in range(24):
             count = sum((num >> bit) & 1 for num in candidates)
             max_combination = max(max_combination, count)
         return max_combination
